8ac8f0dba3c0151f6bd4385b5eaac748b3ce01bb
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2023-06-16 01:56:13 +0300

    Add music on button "Plunge into the Forest"

---
+++ js/music.js
@@ -0,0 +1,14 @@
+        let audio = new Audio();
+        audio.volume = 0.25;
+
+        document.querySelector('#play').addEventListener('click', function (e) {
+          if (e.target.value === 'Plunge into the Forest') {
+            e.target.value = 'Get back from the forest';
+            audio.src = e.target.getAttribute('data-url');
+            audio.play();
+          } else {
+            e.target.value = 'Plunge into the Forest';
+            audio.currentTime = 0;
+            audio.pause();
+          }
+        });
