307f88cd7c3aea3038a88713bea73290b8724e23
Ephemerian <106543055+Ephemerian@users.noreply.github.com>
2023-06-16 03:52:56 +0300

    Added an "ended" event handler to the audio object

--- js/music.js
+++ js/music.js
@@ -1,14 +1,18 @@
-        let audio = new Audio();
-        audio.volume = 0.25;
+let audio = new Audio();
+audio.volume = 0.25;
 
-        document.querySelector('#play').addEventListener('click', function (e) {
-          if (e.target.value === 'Plunge into the Forest') {
-            e.target.value = 'Get back from the forest';
-            audio.src = e.target.getAttribute('data-url');
-            audio.play();
-          } else {
-            e.target.value = 'Plunge into the Forest';
-            audio.currentTime = 0;
-            audio.pause();
-          }
-        });
+document.querySelector('#play').addEventListener('click', function (e) {
+  if (e.target.value === 'Plunge into the Forest') {
+    e.target.value = 'Get back from the forest';
+    audio.src = e.target.getAttribute('data-url');
+    audio.play();
+  } else {
+    e.target.value = 'Plunge into the Forest';
+    audio.currentTime = 0;
+    audio.pause();
+  }
+});
+
+audio.addEventListener('ended', function() {
+  document.querySelector('#play').value = 'Plunge into the Forest';
+});
